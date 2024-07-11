8b4bc33ab73862fa609e6c4ad994050cd3978b8c
Marceillo De Wet <marceillo@hotmail.com>
2024-07-11 11:00:54 +0000

    changed the debug to false

--- kayak_blog/settings.py
+++ kayak_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k^r292g8v7!n0aay&qkle!t+9j89wwt2-7akh271wql5a17d@!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-marceillo-kayakblogpp4-7k6a7fjpl13.ws.codeinstitute-ide.net']
 
