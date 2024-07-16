7bae4a0f4e14fae2cf10a0fa58515dfce66e882f
Marceillo De Wet <marceillo@hotmail.com>
2024-07-16 14:04:03 +0000

    Add the comment model to blog.app and admin file

--- blog/models.py
+++ blog/models.py
@@ -30,3 +30,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['created'] 
+    
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
