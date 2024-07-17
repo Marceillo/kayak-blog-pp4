a4388a36d060a5e0d715516d661f07ca70728564
Marceillo De Wet <marceillo@hotmail.com>
2024-07-17 15:07:52 +0000

    fix a indentation error in blog models

--- blog/models.py
+++ blog/models.py
@@ -68,4 +68,5 @@ class UserProfile (models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-    instance.userprofile.save()    
+    else:
+        instance.userprofile.save()    
