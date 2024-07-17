334918f5fe64c8138a1a4242cc9652b157f484c7
Marceillo De Wet <marceillo@hotmail.com>
2024-07-17 14:30:59 +0000

    Add comment and user model to blog plus update admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, UserProfile
 
 # Register your models here.
 
@@ -13,5 +13,24 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'created', 'approved']
+    list_filter = ['approved', 'created']
+    search_fields = ['author__username', 'body']
+    actions  = ['approve_comments']
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+    approve_comments.short_description = "approve selected comments"
 
-admin.site.register(Comment)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'location', 'experience_level',]
+    list_filter = ['location','experience_level']
+    search_fields = ['user__username', 'first_name', 'last_name', 'location', 'bio']
+    fields = ['user', 'first_name', 'last_name', 'location', 'experience_level','favorite_gear','bio']
+    
+
+
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 
@@ -47,3 +50,22 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
+
+
+class UserProfile (models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    first_name = models.CharField("First Name",max_length=50, null=True, blank=True)
+    last_name =  models.CharField("Last Name",max_length=50, null=True, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    experience_level = models.CharField(max_length=20, blank=True)
+    favorite_gear = models.TextField(blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()    
