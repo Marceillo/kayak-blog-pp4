b720e24e8d9af106c2c56389655f14b6d7914d07
Marceillo De Wet <marceillo@hotmail.com>
2024-07-18 09:57:18 +0000

    Copy Alluth login  templates to dir

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 
 def home(request):
-    return render(request,'blog/index.html')
+    posts = Post.objects.filter(status=Post.PostStatus.PUBLISHED).order_by('-publish')
+    return render(request,'blog/index.html', {'posts': posts})
 
 def about(request):
     return render(request, 'blog/about.html')    
