7abd35f312b6b47e906a334166b438838b27bd14
Marceillo De Wet <marceillo@hotmail.com>
2024-07-19 11:07:56 +0000

    Add style/functionality to Userprofile feature update html files views/urls/forms in blog app

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('user_profile/', views.userprofile, name='user_profile')
+    path('user_profile/', views.userprofile, name='user_profile'),
+    path('profile_edit/',views.profile_edit, name='profile_edit')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post, UserProfile
+from .forms import UserProfileForm
 
 
 # Create your views here.
@@ -10,14 +11,28 @@ def home(request):
     return render(request,'blog/index.html', {'posts': posts})
 
 def about(request):
-    return render(request, 'blog/about.html') 
+    return render(request, 'blog/about.html')
+
 
 @login_required
 def userprofile(request):
-    return render(request, 'blog/user_profile.html')
     profile,created = UserProfile.objects.get_or_create(user=request.user)
     context = {
         'user': request.user,
         'profile': profile
     }
-    return render (request, 'blog/user_profile.html', context)
+    return render(request, 'blog/user_profile.html', context)
+
+@login_required
+def profile_edit(request):
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('user_profile')
+    
+    else:
+        form = UserProfileForm(instance=profile)
+
+    return render (request, 'blog/profile_edit.html', {'form': form})
