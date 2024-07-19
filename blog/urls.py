28d5f1003b48189b2619f4fc46d71b84f8149b32
Marceillo De Wet <marceillo@hotmail.com>
2024-07-19 07:37:56 +0000

    Ad  initial User profile feature  for after user logins

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views 
 
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about'),
+    path('about/', views.about, name='about'),
+    path('user_profile/', views.userprofile, name='user_profile')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Post
+from django.contrib.auth.decorators import login_required
+from .models import Post, UserProfile
 
 
 # Create your views here.
@@ -9,4 +10,14 @@ def home(request):
     return render(request,'blog/index.html', {'posts': posts})
 
 def about(request):
-    return render(request, 'blog/about.html')    
+    return render(request, 'blog/about.html') 
+
+@login_required
+def userprofile(request):
+    return render(request, 'blog/user_profile.html')
+    profile,created = UserProfile.objects.get_or_create(user=request.user)
+    context = {
+        'user': request.user,
+        'profile': profile
+    }
+    return render (request, 'blog/user_profile.html', context)
