863fa862bf6abaca788eed7fd8643b3f924c05af
Marceillo De Wet <marceillo@hotmail.com>
2024-08-02 07:46:15 +0000

    Change the ecerpt save form so that is shows the ... at the end when the text is longer than 200 and added code for when the text is shorter for the automatic generate.

--- blog/forms.py
+++ blog/forms.py
@@ -28,7 +28,8 @@ class PostForm(forms.ModelForm):
     def save (self, commit=True ):
         instance = super().save(commit=False)
         if not instance.excerpt:
-            instance.excerpt = instance.body[:200] + ''
+            instance.excerpt = instance.body[:197] + '...'
+            if len(instance.body) > 197 else instance.body
         if commit:
             instance.save()
             return instance
