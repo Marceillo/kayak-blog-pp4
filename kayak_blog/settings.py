2d3c4a6500de6d95e20f53a25ccb857495f2ec42
Marceillo De Wet <marceillo@hotmail.com>
2024-08-06 09:26:25 +0000

    change debug to false

--- kayak_blog/settings.py
+++ kayak_blog/settings.py
@@ -30,13 +30,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 #SECRET_KEY = 'django-insecure-k^r292g8v7!n0aay&qkle!t+9j89wwt2-7akh271wql5a17d@!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-marceillo-kayakblogpp4-7k6a7fjpl13.ws.codeinstitute-ide.net','kayak-blog-pp4-1054055911f7.herokuapp.com']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
