3850a2a805b9dd2a02ebc623aea943d3dfdd61f0
Marceillo De Wet <marceillo@hotmail.com>
2024-08-15 13:56:34 +0000

    Start build for the about app not yet functional

--- about/urls.py
+++ about/urls.py
@@ -1,6 +1,8 @@
-from . import views 
-from django.urls import path 
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path('', views.about_kayaking, name='about_kayaking'),
+    path('contact/', about_views.contact_us, name='contact'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 
 
 # Create your views here.
 def about_kayaking(request):
     return render(request,'about/about.html')
+
+def contact_us (request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            return redirect('about:contact_success')
+            
+        else:
+            form = ContactForm()
+            return rendar(request, 'about/contact_success.html', {'form': form})
+
+def contact_success(request):
+    return render(request, 'about/contact_success.html') 
+            
